134b80ffd617a7f89419bc3e6efa3b6794675825
haganmao <kevin_mmc@hotmail.com>
2020-09-13 18:19:33 +1200

    update project

---
+++ NZJobFInder/Controllers/FeedbackController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NZJobFInder.Models;
+namespace NZJobFInder.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        // GET: Feedback
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string feedpass(Feedback feedback) {
+
+            try
+            {
+                Database1Entities db = new Database1Entities();
+                db.Feedbacks.Add(feedback);
+                db.SaveChanges();
+                return "feedback saved success";
+            }
+            catch {
+                return "feedback saved fail";
+            }
+        }
+    }
+}
---
+++ NZJobFInder/Controllers/JobController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NZJobFInder.Models;
+namespace NZJobFInder.Controllers
+{
+    public class JobController : Controller
+    {
+        // GET: Job
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        //save job details to db
+        public string jobpass(Job job) {
+
+            try {
+                Database1Entities db = new Database1Entities();
+                db.Jobs.Add(job);
+                db.SaveChanges();
+                return "add success!";
+            }
+            catch
+            {
+                return "add failed, please check your employer id first";
+            }
+
+        }
+
+    }
+}

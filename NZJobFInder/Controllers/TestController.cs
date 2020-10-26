a1c73945e164afc8b5d953741a57398b5eb07e76
haganmao <haganmao@gmail.com>
2020-10-26 19:15:05 +1300

    add DATAANNOTATION

---
+++ NZJobFInder/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NZJobFInder.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+
+            //assign a list of string to a list  
+            var students = new List<string> { "Wang", "Gao", "Mao" };
+
+            ViewBag.stus = students;
+
+            return View();
+        }
+    }
+}

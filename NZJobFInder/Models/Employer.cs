3657fe09f1f35fbf3459bd282ef1c28af8160346
haganmao <haganmao@gmail.com>
2020-10-26 20:22:17 +1300

    add form validation in employer registration

--- NZJobFInder/Models/Employer.cs
+++ NZJobFInder/Models/Employer.cs
@@ -11,7 +11,7 @@ namespace NZJobFInder.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
     public partial class Employer
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,11 +21,21 @@ namespace NZJobFInder.Models
         }
     
         public int employerId { get; set; }
+
+
+        [Required(ErrorMessage = "Employer name is required"),MinLength(3),MaxLength(10)]
         public string employerName { get; set; }
         public string employerAddress { get; set; }
+
+        [Required(ErrorMessage = "Phone is required"),MinLength(8),MaxLength(12)]
         public string employerPhoneNum { get; set; }
         public string employerUserName { get; set; }
         public string employerPassword { get; set; }
+
+
+        [Required(ErrorMessage = "email is required")]
+        [StringLength(20, ErrorMessage = "email must be less than 20 characters" )]
+        [RegularExpression(@"^[a-zA-Z0-9]+@[a-zA-Z0-9]+(\.[a-zA-Z]+)*$", ErrorMessage ="Email is not valid")]
         public string employerEmailAddress { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
